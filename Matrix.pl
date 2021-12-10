27eb99e1297cad540478202aa7474f4603dcc9ad
Mohamed Sami <ma-771@hotmail.coom>
2021-12-11 00:02:33 +0200

    updating the successor state function

--- Matrix.pl
+++ Matrix.pl
@@ -1,7 +1,7 @@
 :- include('KB').
 
-
-neo_loc(X,Y,S):-
+neo_loc(_,_,s0,[],[]).
+neo_loc(X,Y,S,CH,DH):-
 	(
 		A = up,
 		U is X+1,
@@ -9,7 +9,7 @@ neo_loc(X,Y,S):-
 		U < GX,
 		X >= 0,
 		S = result(A, S1),
-		neo_loc(U,Y,S1)
+		neo_loc(U,Y,S1,CH,DH)
 	);
 	(
 		A = down,
@@ -18,7 +18,7 @@ neo_loc(X,Y,S):-
 		D >= 0,
 		X < GX,
 		S = result(A, S1),
-		neo_loc(D,Y,S1)
+		neo_loc(D,Y,S1,CH,DH)
 	);
 	(
 		
@@ -28,7 +28,7 @@ neo_loc(X,Y,S):-
 		grid(_,GY),
 		Y < GY,
 		S = result(A,S1),
-		neo_loc(X,R,S1)
+		neo_loc(X,R,S1,CH,DH)
 	);
 	(
 		A = left,
@@ -37,17 +37,38 @@ neo_loc(X,Y,S):-
 		L < GY,
 		GY >= 0,
 		S = result(A,S1),
-		neo_loc(X,L,S1)
+		neo_loc(X,L,S1,CH,DH)
 	);
 	(
 		A = carry,
-		
+		hostages_loc([[[HX,HY]]|HT]),
+		X == HX,
+		Y == HY,
+		length(CH,NH),
+		capacity(C),
+		NH < C,
+		S = result(A,S1),
+		neo_loc(X,Y,S1,[CH|[[HX,HY]]],DH)
 	);
 	(
 		A = drop,
-		
-	)
+		booth(BX,BY),
+		X == BX,
+		Y == BY,
+		length(CH,LH),
+		LH > 0,
+		S = result(A,S1),
+		neo_loc(X,Y,S1,[],[[CH]|[DH]])
+	).
 	
 goal(S):-
 	neo_loc(X,Y),
-	S = result(A, S1)
+	neo_loc(X,Y,S,CH,DH),
+	booth(BX,BY),
+	X == BX, Y == BY,
+	hostages_loc(H),
+	length(H,LH1), length(DH,LH2), LH1 == LH2, 
+	print(LH1).
+	
+
+	
