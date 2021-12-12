d13e4e8c82df817bf903b3838a8c7369a1017950
Mohamed Sami <ma-771@hotmail.coom>
2021-12-13 00:32:22 +0200

    Updated successor state axioms

--- Matrix.pl
+++ Matrix.pl
@@ -1,79 +1,52 @@
 :- include('KB').
-neo_loc(X,Y,s0,H,[],[]):- neo_loc(X,Y), hostages_loc(H).
-neo_loc(X,Y,S,H,CH,[DH|DT]):-
-	(
-		A = drop,
-		booth(X,Y),
-		length(CH,NC),
-		capacity(C),
-		NC < C,
-		append([DH],CH,NCH),
-		S = result(A,S1),
-		neo_loc(X,Y,S1,H,NCH,DT)
-	);
-	(
-		A = carry,
-		cancarry(CH,[X,Y]),	
-		
-		length(CH,NC),
-		capacity(C),
-		NC =< C,
 
-		remove([X,Y],CH,NCH),
-		append([[X,Y]],H,NH),
-		S = result(A,S1),
-		neo_loc(X,Y,S1,NH,NCH,[DH|DT])
-	);
+neo_loc(X,Y,s0,C,H):- neo_loc(X,Y), capacity(C), hostages_loc(H).
+neo_loc(X,Y,result(A,S1),C,H):-
 	(
-		A = up,
-		U is X+1,
-		grid(GX,_),
-		U < GX,
-		X >= 0,
-		S = result(A,S1),
-		neo_loc(U,Y,S1,H,CH,[DH|DT])
-	);
-	(
+		neo_loc(D,Y,S1,C,H),
 		A = down,
-		D is X-1,
+		X is D+1,
 		grid(GX,_),
 		D >= 0,
-		X < GX,
-		S = result(A,S1),
-		neo_loc(D,Y,S1,H,CH,[DH|DT])
+		D < GX
 	);
 	(
-		
+		neo_loc(U,Y,S1,C,H),
+		A = up,
+		X is U-1,
+		grid(GX,_),
+		U >= 0,
+		U < GX
+	);
+	(
+		neo_loc(X,R,S1,C,H),
 		A = right,
-		R is Y-1,
-		R >= 0,
+		Y is R+1,
 		grid(_,GY),
-		Y < GY,
-		S = result(A,S1),
-		neo_loc(X,R,S1,H,CH,[DH|DT])
+		R < GY
 	);
 	(
-		A = left,
-		L is Y+1,
+		neo_loc(X,L,S1,C,H),
+		A = left, 
+		Y is L-1,
 		grid(_,GY),
-		L < GY,
-		GY >= 0,
-		S = result(A,S1),
-		neo_loc(X,L,S1,H,CH,[DH|DT])
+		L >= 0,
+		L < GY
+	);
+	(
+		neo_loc(X,Y,S1,OC,H1),
+		A = carry,
+		member([X,Y],H1),
+		delete(H1,[X,Y],H),
+		C is OC - 1,
+		OC > 0
+	);
+	(
+		neo_loc(X,Y,S1,_,H),
+		A = drop,
+		capacity(C)
 	).
-
 	
-cancarry([[H1,H2]], [H1, H2]).
-cancarry([[H1,H2],_|_], [H1, H2]).
-cancarry([[_,_],H3|T], [X, Y]) :- cancarry([H3|T], [X, Y]).
-
-remove([X,Y],[],[]).
-remove([X,Y],[[X,Y]|T],L2):- remove([X,Y],T,L2).
-remove([X,Y],[[A,B]|T],[[A,B]|S]):- 
-	[X,Y]\=[A,B],
-	remove([X,Y],T,S). 
-
 goal(S):-
 	booth(X,Y),
-	hostages_loc(DH),
-	neo_loc(X,Y,S,[],[],DH).
+	neo_loc(X,Y,S,1,[]).
